03c213840ffa81e1d2fa250a38e8104057583e2b
Joel Gardner <joel@simondata.com>
2025-02-18 09:10:57 -0600

    sync with tap-klaviyo-temporary repo

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 
 from setuptools import setup
 
-setup(name='tap-klaviyo',
+setup(name='tap-klaviyo-temporary',
       version='0.0.1',
       description='Singer.io tap for extracting data from the Klaviyo API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_klaviyo'],
-      install_requires=['singer-python==3.2.1',
+      install_requires=['singer-python==5.12.2',
                         'requests==2.13.0',
-                        'backoff',
+                        'backoff==1.8.0',
                         'ipdb'],
       entry_points='''
           [console_scripts]
-          tap-klaviyo=tap_klaviyo:main
+          tap-klaviyo-temporary=tap_klaviyo:main
       ''',
       packages=['tap_klaviyo'],
       package_data={
@@ -28,11 +28,17 @@ setup(name='tap-klaviyo',
                 "unsubscribe.json",
                 "dropped_email.json",
                 "global_exclusions.json",
+                "global_exclusions2.json",
                 "lists.json",
+                "lists2.json",
                 "list_members.json",
+                "list_members2.json",
+                "metrics2.json",
                 "subscribe_list.json",
                 "unsub_list.json",
                 "update_email_preferences.json",
+                "events.json",
+                "profiles.json"
               ]
          },
       include_package_data=True
